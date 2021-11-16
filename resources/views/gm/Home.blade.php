f1db032635817c19bb4d7f45c54adb99082e869a
MarioCJorge <2201258@my.ipleiria.pt>
2021-11-16 19:04:28 +0000

    atualizacao

---
+++ resources/views/gm/Home.blade.php
@@ -0,0 +1,84 @@
+@extends("layout.master")
+
+@section("title")
+    Home
+@endsection
+
+@section("estilos")
+<link rel="stylesheet" type="text/css" href="{{asset('css/home.css')}}">
+@endsection
+
+@section("content")
+<div class="position-relative" >
+<img id="banner" src="../fotos/Banner.png" alt="">
+
+<h1 id="title" class=" position-absolute rounded-pill" > ELES PRECISÃO DE TI</h1>
+</div>
+
+<div class="vazio"></div>
+
+<div class="conteiner">
+
+<h2 id="title2"class="text-center">Já Adotados</h2>
+
+</div>
+
+<div class="vazio"></div>
+
+ <div class="container-fluid">
+
+    <div id class="row">
+      
+        <div class=" col-4 text-center">
+
+        <img  class="imagens rounded-circle align-middle" src="../fotos/cat.jpg" alt="">
+
+        <p>ja foram adotados</p>
+
+        <h3>36 Gatos</h3>
+
+     
+
+        </div>
+
+        <div class="col-4 text-center">
+
+        <img  class="imagens rounded-circle align-middle" src="../fotos/cão.jpg" alt="">
+        
+        <p>ja foram adotados</p>
+
+        <h3>24 Cães</h3>
+   
+      
+
+        </div>
+
+        <div class=" col-4 text-center">
+
+        <img  class="imagens rounded-circle align-middle" src="../fotos/bird.jpg" alt="">
+        
+        <p>ja foram adotados</p>
+
+        <h3>20 Aves</h3>
+
+        
+
+        </div>
+
+    </div>
+
+ </div>
+
+ <div class="vazio"></div>
+
+<div class="conteiner">
+
+<h2 id="title2"class="text-center">Eventos</h2>
+
+</div>
+
+<div class="vazio"></div>
+
+<div style=" height :2000px;">
+</div>
+@endsection
