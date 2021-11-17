7a535ae9c5ffb1194675a31799f17385c2bc778f
João Domingos <2201266@my.ipleiria.pt>
2021-11-17 08:44:35 +0000

    Rotas Corrigidas

--- resources/views/gm/apoios.blade.php
+++ resources/views/gm/apoios.blade.php
@@ -16,7 +16,6 @@
     <div class="row">
             <div class="col-md-4">
                 <div class="cartao">
-                    aaa
                     <div class="imgpatro_arcana">
                         <img src="fotos/acana.png" alt="Patrocinio">
                     </div>
@@ -104,6 +103,7 @@
             </div>
         </div>
     </div>
+    <div class="vazio"></div>
     
 </body>
 @endsection
