06d4289a5d9b10223a42d8c1ee7d21ef4e3f99ae
Mário <2201258@my.ipleiria.pt>
2021-11-18 15:17:46 +0000

    atualizacao home

--- resources/views/gm/Home.blade.php
+++ resources/views/gm/Home.blade.php
@@ -12,7 +12,7 @@
 
 <div class="conteiner">
 
-<h2 id="title2"class="text-center">Já Adotados</h2>
+<h2 class="text-center title2">Já Adotados</h2>
 
 </div>
 
@@ -22,38 +22,38 @@
 
     <div class="row">
       
-        <div class="  col-12 col-md-4 text-center">
+        <div class="  col-4 text-center">
 
-        <img  class="imagens rounded-circle align-middle" src="fotos/cat.jpg" alt="">
+        <img  class="border border-dark imagens rounded-circle align-middle" src="fotos/cat.jpg" alt="">
 
-        <p>ja foram adotados</p>
+        <p class=" texto ">ja foram adotados</p>
 
-        <h3>36 Gatos</h3>
+        <h3 class=" title3 ">36 Gatos</h3>
 
      
 
         </div>
 
-        <div class="  col-12 col-md-4 text-center">
+        <div class="  col-4 text-center">
 
-        <img  class="imagens rounded-circle align-middle" src="fotos/cao.jpg" alt="">
+        <img  class="border border-dark imagens rounded-circle align-middle" src="fotos/cao.jpg" alt="">
         
-        <p>ja foram adotados</p>
+        <p  class=" texto ">ja foram adotados</p>
 
-        <h3>24 Cães</h3>
+        <h3 class=" title3 ">24 Cães</h3>
   
    
       
 
         </div>
 
-        <div class="  col-12 col-md-4 text-center">
+        <div class="   col-4 text-center">
 
-        <img  class="imagens rounded-circle align-middle" src="fotos/bird.jpg" alt="">
+        <img  class="border border-dark imagens rounded-circle align-middle" src="fotos/bird.jpg" alt="">
         
-        <p>ja foram adotados</p>
+        <p class=" texto ">ja foram adotados</p>
 
-        <h3>20 Aves</h3>
+        <h3 class=" title3 ">20 Aves</h3>
 
         
 
@@ -63,26 +63,31 @@
 
  </div>
 
+ <div class="vazio"></div>
+
  <div class="conteiner">
      
-<h2 id="title2"class="text-center">Eventos</h2>
+<h2 class="text-center title2">Eventos</h2>
 
 </div>
+<div class="vazio"></div>
 
-<div class="border border-dark container-fluid">
 
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+<div class="border border-dark w-75 mx-auto bg-orange rounded">
+
+ <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
   </ol>
   <div class="carousel-inner">
     <div class="carousel-item active">
 
-      <div class=" d-block w-100 container-fluid">
+     <div class=" d-block w-100 container-fluid">
 
-    <div class="row">
+      <div class="row">
       
         <div class="  col-12 col-md-6 ">
 
@@ -105,17 +110,109 @@
 
         </div>
 
-    </div>
+      </div>
 
- </div>
+     </div>
 
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="fotos/cao.jpg" alt="Second slide">
+     <div class=" d-block w-100 container-fluid">
+
+      <div class="row">
+
+       <div class="  col-12 col-md-6 ">
+
+  
+
+  <h3>Evento</h3>
+  <p>venha conhecer um amigo</p>
+  <p>09H - 13H | 12/11/2021 | Campus 2 Morro do Lena, 2411-901, Leiria  </p>
+  <p>venha conhecer um amigo</p>
+  <p>venha conhecer um amigo</p>
+
+  
+
+       </div>
+  
+       <div class="  col-12 col-md-6 text-center ">
+  <img  class=" imagens-evento align-middle" src="fotos/cat.jpg" alt="">
+  
+
+
+       </div>
+
+      </div>
+
+     </div>
     </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="fotos/bird.jpg" alt="Third slide">
+  <div class="carousel-item">
+
+    <div class=" d-block w-100 container-fluid">
+
+      <div class="row">
+
+       <div class="  col-12 col-md-6 ">
+
+
+
+<h3>Evento</h3>
+<p>venha conhecer um amigo</p>
+<p>09H - 13H | 12/11/2021 | Campus 2 Morro do Lena, 2411-901, Leiria  </p>
+<p>venha conhecer um amigo</p>
+<p>venha conhecer um amigo</p>
+
+
+
+       </div>
+
+       <div class="  col-12 col-md-6 text-center ">
+<img  class=" imagens-evento align-middle" src="fotos/cao.jpg" alt="">
+
+
+
+       </div>
+
+      </div>
+
     </div>
+
+   
+
+  </div>
+
+  <div class="carousel-item">
+
+    <div class=" d-block w-100 container-fluid">
+
+      <div class="row">
+
+       <div class="  col-12 col-md-6 ">
+
+
+
+<h3>Evento</h3>
+<p>venha conhecer um amigo</p>
+<p>09H - 13H | 12/11/2021 | Campus 2 Morro do Lena, 2411-901, Leiria  </p>
+<p>venha conhecer um amigo</p>
+<p>venha conhecer um amigo</p>
+
+
+
+       </div>
+
+       <div class="  col-12 col-md-6 text-center ">
+<img  class=" imagens-evento align-middle" src="fotos/bird.jpg" alt="">
+
+
+
+       </div>
+
+      </div>
+
+    </div>
+
+   
+
   </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -129,7 +226,9 @@
 
 </div>
 
+</div>
+
+
 <div class="vazio"></div>
 
-<div style=" height :2000px;"></div>
 @endsection
