c4b651b2fe22308632f0bf17ad96eca45cdea42b
João Domingos <2201266@my.ipleiria.pt>
2021-11-18 14:04:08 +0000

    Contactos

--- resources/views/layout/partials/navbar.blade.php
+++ resources/views/layout/partials/navbar.blade.php
@@ -7,7 +7,7 @@
 <nav>
     <ul>
         <div class="navesq @if ($menuOption=='I') active @endif">
-        <li><a href="#"> PetStars </a></li>
+        <li class="@if ($menuOption=='O') active @endif"><a href="{{route('gm.contactos')}}"> PetStars </a></li>
         <li class="@if ($menuOption=='B') active @endif"><a href="{{route('gm.adocao')}}"> Adoção </a></li>
         <li class="@if ($menuOption=='D') active @endif"><a href="{{route('gm.faq')}}"> Faqs </a></li>
         <li class="@if ($menuOption=='C') active @endif"><a href="{{route('gm.apoios')}}"> Apoios </a></li>
