5d13335c24db9c127b28f84bc7dd82f8ab61ecf9
DamiaoGranada <2201254@my.ipleiria.pt>
2021-11-18 17:52:06 +0000

    Modificaçao em alguns ficheiros

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
  <?php
 
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\PageController;
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +23,15 @@ Route::get('/adocao', [PageController::class,'adocao'])->name('gm.adocao');
 Route::get('/faq', [PageController::class,'faq'])->name('gm.faq');
 
 Route::get('/contactos', [PageController::class,'contactos'])->name('gm.contactos');
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes(['register' => false, 'verify' => true]);
+
+Route::group(['middleware' => ['auth', 'verified']], function () {
+    Route::get('/users/{user}/send_reactivate_mail',
+    [UserController::class,'send_reactivate_email'])->name('users.sendActivationEmail');
+    Route::resource('posts',PostController::class);
+    Route::resource('categories',CategoryController::class);
+    Route::resource('users',UserController::class);
+   });
