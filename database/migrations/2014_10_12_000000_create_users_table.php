0c510e6803fdc91d2218f04b44a400cddf401996
João Domingos <2201266@my.ipleiria.pt>
2021-11-24 09:57:29 +0000

    Login Funcionar

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,26 +14,25 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id()->nullable(false);
-            $table->string('p_nome', 25)->nullable(false);
-            $table->string('apelido', 25)->nullable(false);
-            $table->char('nif', 9)->unique()->nullable(false);
-            $table->string('login', 50)->nullable(false);
-            $table->string('password', 50)->nullable(false);
-            $table->enum('funcao', array('admin', 'cliente'))->nullable(false);
-            $table->string('localidade')->nullable(false);
-            $table->char('cod_postal', 8)->nullable(false);
-            $table->string('rua')->nullable(false);
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->string('apelido', 25)->nullable(TRUE);
+            $table->char('nif', 9)->unique()->nullable(TRUE);
+            $table->enum('funcao', array('admin', 'cliente'))->nullable(TRUE);
+            $table->string('localidade')->nullable(TRUE);
+            $table->char('cod_postal', 8)->nullable(TRUE);
+            $table->string('rua')->nullable(TRUE);
             $table->char('telefone', 9)->unique()
-                                        ->nullable(false);
-            $table->string('email')->unique()
-                                    ->nullable(false);
+                                        ->nullable(TRUE);
 
 
             
-            $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
-            $table->timestamps();
+            
         });
     }
 
