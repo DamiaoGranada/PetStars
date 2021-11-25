24c7949aca2d2c3bb0864e295c6bf2df3653427e
Mário <2201258@my.ipleiria.pt>
2021-11-25 14:11:32 +0000

    atualizacao navbar

--- resources/views/layout/partials/navbar.blade.php
+++ resources/views/layout/partials/navbar.blade.php
@@ -1,18 +1,16 @@
 <link href="{{asset('css/navbar.css')}}" rel="stylesheet">
 
-<div class="logobar @if ($menuOption=='E') active @endif">
-    <a href="{{route('gm.Home')}}"><img id="navimg" src="img/Logotipo_Final.png"></a>
-</div>
+
 
 <nav>
     <ul>
-        <div class="navesq @if ($menuOption=='I') active @endif">
+    <li class="logo bar @if ($menuOption=='E') active @endif"> <a href="{{route('gm.Home')}}"><img id="navimg" src="img/Logotipo_Final.png"></a></li>
         <li class="@if ($menuOption=='O') active @endif"><a href="{{route('gm.contactos')}}"> PetStars </a></li>
         <li class="@if ($menuOption=='B') active @endif"><a href="{{route('gm.adocao')}}"> Adoção </a></li>
         <li class="@if ($menuOption=='D') active @endif"><a href="{{route('gm.faq')}}"> Faqs </a></li>
         <li class="@if ($menuOption=='C') active @endif"><a href="{{route('gm.apoios')}}"> Apoios </a></li>
         <li><a href="#"> Loja </a></li>
         <li><a href="#"> Login </a></li>
-        </div>
+        
     </ul>
 </nav>
