5be7dd1a35e7a1e247e3da65ed32d57c3325d724
Mário <2201258@my.ipleiria.pt>
2021-11-29 16:46:00 +0000

    atualizacao home dinamica

--- resources/views/gm/Home.blade.php
+++ resources/views/gm/Home.blade.php
@@ -7,6 +7,50 @@ Home
 @endsection
 
 @section ('content')
+<?php
+    $host="localhost" ;
+    $user= "root";
+    $pass= "";
+    $database= "petstars";
+
+    $conn = new mysqli($host, $user, $pass, $database);
+    if ($conn->connect_errno) {$code = $conn->connect_errno;$message = $conn->connect_error;
+        printf("<p>Connection error: %d %s</p>", $code, $message);}
+        else{
+          echo "correto";
+        }
+
+        $sql = "select * FROM eventos INNER JOIN foto on eventos.id=foto.id_eventos";
+        $resultado = $conn->query($sql);
+    
+        $ide = array();
+        $nome = array();
+        $descricao = array();
+        $data = array();
+        $tipo = array();
+        $localidade = array();
+        $caminho = array();
+        
+    
+        if ($resultado->num_rows > 0) {
+            while($row = $resultado->fetch_assoc()) {
+              $ide[]=$row["id"]; 
+              $nome[]=$row["nome_evento"];
+                $descricao[]=$row["descri_evento"]; 
+                $data[]=$row["data_evento"]; 
+                $tipo[]=$row["tipo_evento"];  
+                $localidade[]=$row["local_evento"];
+                $caminho[]=$row["caminho"];
+                
+            }
+        } else {
+            echo "0 results";
+        }
+
+       
+
+        ?>
+
 <link rel="stylesheet" type="text/css" href="{{asset('css/home.css')}}">
 <div class="position-relative" >
 <img id="banner" src="../fotos/Banner.png" alt="">
@@ -83,139 +127,68 @@ Home
 
  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+  <?php
+
+  for($x=0; $x < $resultado->num_rows; $x++){
+
+    if ($x == 0){
+      $active ="active";
+      }else{
+        $active ="";
+      };
+
+    echo ' <li data-target="#carouselExampleIndicators" data-slide-to="'.$x; echo '" class="'.$active; echo'"></li> ';
+  }
+    ?>
+
   </ol>
   <div class="carousel-inner">
-  <div class="carousel-item active">
+    <?php
+    
+            for($x=0; $x < $resultado->num_rows; $x++){
+              if ($x == 0){
+              $active ="active";
+              }else{
+                $active ="";
+              };
+ 
+              echo '<div class="carousel-item '.$active;echo '">
 
-     <div class=" d-block w-100 container-fluid">
+              <div class=" d-block w-100 container-fluid">
+         
+               <div class="row">
+               
+                 <div class="  col-12 col-md-6 ">
+         
+                 
+         
+                 <h3> '.$nome[$x]; echo '</h3>
+                 <p>  '.$descricao[$x]; echo'</p>
+                 <p> '.$data[$x]; echo'  </p>
+                 <p> '.$tipo[$x]; echo'</p>
+                 <p>'.$localidade[$x]; echo'</p>
+         
+                
+            
+                 </div>
+                 
+                 <div class="  col-12 col-md-6 text-center ">
+                 <img  class=" imagens-evento align-middle" src="'.$caminho[$x];echo'" alt="">
+                 
+         
+         
+                 </div>
+         
+               </div>
+         
+              </div>
+         
+           </div>';
+            }
+    
+            ?>
+ 
 
-      <div class="row">
-      
-        <div class="  col-12 col-md-6 ">
-
-        
-
-        <h3>Evento</h3>
-        <p>venha conhecer um amigo</p>
-        <p>09H - 13H | 12/11/2021 | Campus 2 Morro do Lena, 2411-901, Leiria  </p>
-        <p>venha conhecer um amigo</p>
-        <p>venha conhecer um amigo</p>
-
-        
-
-        </div>
-        
-        <div class="  col-12 col-md-6 text-center ">
-        <img  class=" imagens-evento align-middle" src="fotos/cao.jpg" alt="">
-        
-
-
-        </div>
-
-      </div>
-
-     </div>
-
-  </div>
-  <div class="carousel-item">
-     <div class=" d-block w-100 container-fluid">
-
-      <div class="row">
-
-       <div class="  col-12 col-md-6 ">
-
-  
-
-        <h3>Evento</h3>
-        <p>venha conhecer um amigo</p>
-        <p>09H - 13H | 12/11/2021 | Campus 2 Morro do Lena, 2411-901, Leiria  </p>
-        <p>venha conhecer um amigo</p>
-        <p>venha conhecer um amigo</p>
-
-  
-
-       </div>
-  
-       <div class="  col-12 col-md-6 text-center ">
-        <img  class=" imagens-evento align-middle" src="fotos/cat.jpg" alt="">
-  
-
-
-       </div>
-
-      </div>
-
-     </div>
-  </div>
-  <div class="carousel-item">
-
-    <div class=" d-block w-100 container-fluid">
-
-      <div class="row">
-
-       <div class="  col-12 col-md-6 ">
-
-
-
-        <h3>Evento</h3>
-        <p>venha conhecer um amigo</p>
-        <p>09H - 13H | 12/11/2021 | Campus 2 Morro do Lena, 2411-901, Leiria  </p>
-        <p>venha conhecer um amigo</p>
-        <p>venha conhecer um amigo</p>
-
-
-
-       </div>
-
-       <div class="  col-12 col-md-6 text-center ">
-        <img  class=" imagens-evento align-middle" src="fotos/cao.jpg" alt="">
-
-       </div>
-
-      </div>
-
-    </div>
-
-
-  </div>
-  <div class="carousel-item">
-
-    <div class=" d-block w-100 container-fluid">
-
-      <div class="row">
-
-       <div class="  col-12 col-md-6 ">
-
-
-
-        <h3>Evento</h3>
-        <p>venha conhecer um amigo</p>
-        <p>09H - 13H | 12/11/2021 | Campus 2 Morro do Lena, 2411-901, Leiria  </p>
-        <p>venha conhecer um amigo</p>
-        <p>venha conhecer um amigo</p>
-
-
-
-       </div>
-
-       <div class="  col-12 col-md-6 text-center ">
-        <img  class=" imagens-evento align-middle" src="fotos/bird.jpg" alt="">
-
-
-
-       </div>
-
-      </div>
-
-    </div>
-
-   
-
-  </div>
   <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
