e274982548e0aea8a237702d6f1d38c200d8cf08
Mário <2201258@my.ipleiria.pt>
2021-12-06 17:22:05 +0000

    atualizacao navbar

--- resources/views/layout/partials/navbar.blade.php
+++ resources/views/layout/partials/navbar.blade.php
@@ -1,16 +1,32 @@
-<link href="{{asset('css/navbar.css')}}" rel="stylesheet">
 
 
+<nav class=" navbar navbar-expand-lg ">
+  <a class=" navbar-brand" href="{{route('gm.Home')}}"><img id="navimg" src="img/Logotipo_Final.png"></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"><img id="navimg" src="img/Logotipo_Final.png"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class=" navbar-nav row">
+      <li class="col nav-item @if ($menuOption=='O') active @endif">
+        <a class="nav-link" href="{{route('gm.contactos')}}">PetStars <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="col nav-item @if ($menuOption=='B') active @endif">
+        <a class="nav-link" href="{{route('gm.adocao')}}">Adoção</a>
+      </li>
+      <li class="col nav-item @if ($menuOption=='D') active @endif">
+        <a class="nav-link" href="{{route('gm.faq')}}">Faqs</a>
+      </li>
+      <li class="col nav-item @if ($menuOption=='C') active @endif">
+        <a class="nav-link" href="{{route('gm.apoios')}}">Apoios</a>
+      </li>
+      <li class="col nav-item ">
+        <a class="nav-link" href="https://lojapetstars.g2.dwm22.space/2021/11/03/hello-world/">Loja</a>
+      </li>
+      <li class="col nav-item">
+        <a class="nav-link" href="{{route('login')}}">Login</a>
+      </li>
 
-<nav>
-    <ul>
-    <li class="logo bar @if ($menuOption=='E') active @endif"> <a href="{{route('gm.Home')}}"><img id="navimg" src="img/Logotipo_Final.png"></a></li>
-        <li class="@if ($menuOption=='O') active @endif"><a href="{{route('gm.contactos')}}"> PetStars </a></li>
-        <li class="@if ($menuOption=='B') active @endif"><a href="{{route('gm.adocao')}}"> Adoção </a></li>
-        <li class="@if ($menuOption=='D') active @endif"><a href="{{route('gm.faq')}}"> Faqs </a></li>
-        <li class="@if ($menuOption=='C') active @endif"><a href="{{route('gm.apoios')}}"> Apoios </a></li>
-        <li><a href="https://lojapetstars.g2.dwm22.space/2021/11/03/hello-world/"> Loja </a></li>
-        <li><a href="{{route('login')}}"> Login </a></li>
-        
     </ul>
+  </div>
 </nav>
+</div>
