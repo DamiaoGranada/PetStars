99ab3044189eb053f38bee675358a6259f5a9efa
João Domingos <2201266@my.ipleiria.pt>
2021-12-09 15:31:08 +0000

    Update

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -20,7 +20,6 @@
             <div class="vazio"></div>
         <div class="container">
           
-        @dump($errors)
 
 
         <form action="{{route('register')}}" method="post" class="form">
@@ -37,7 +36,7 @@
 
 									<label for="name" class="">Nome:</label><br>
 
-			               			<input type="text" name="name" id="first_name" class="form-control input-sm @error('name') is-valid @enderror" value="{{old('name')}}" required autocomplete="name" autofocus placeholder="Insira o primeiro nome">
+			               			<input type="text" name="name" id="first_name" class="form-control input-sm @error('name') is-valid @enderror" value="{{old('name')}}" required autocomplete="name" autofocus placeholder="Isabel">
 
                                        @error("name")
 
@@ -50,7 +49,7 @@
 			    				<div class="col-xs-6 col-sm-6 col-md-6">
 			    					<div class="form-group">
 									<label for="apelido" class="">Apelido:</label><br>
-			    					<input type="text" name="apelido" id="last_name" class="form-control input-sm @error('apelido') is-valid @enderror" value="{{old('apelido')}}" required autocomplete="apelido" autofocus placeholder="Insira a Apelido">
+			    					<input type="text" name="apelido" id="last_name" class="form-control input-sm @error('apelido') is-valid @enderror" value="{{old('apelido')}}" required autocomplete="apelido" autofocus placeholder="Almeida">
                                     @error("apelido")
 
                                     <span class="text-danger" role="alert">
@@ -62,7 +61,7 @@
 			    			</div>
                             <div class="form-group">
 								<label for="localidade" class="">Localidade:</label><br>
-			    				<input type="text" name="localidade" id="localidade" class="form-control input-sm @error('localidade') is-valid @enderror" value="{{old('localidade')}}" required autocomplete="localidade" autofocus placeholder="Insira a sua localidade">
+			    				<input type="text" name="localidade" id="localidade" class="form-control input-sm @error('localidade') is-valid @enderror" value="{{old('localidade')}}" required autocomplete="localidade" autofocus placeholder="Leiria">
                                 @error("localidade")
 
                                 <span class="text-danger" role="alert">
@@ -72,7 +71,7 @@
                             </div>
 							<div class="form-group">
 									<label for="rua" class="">Rua:</label><br>
-			    				    <input type="text" name="rua" id="rua" class="form-control input-sm @error('rua') is-valid @enderror" value="{{old('rua')}}" required autocomplete="rua" autofocus placeholder="Insira a rua">
+			    				    <input type="text" name="rua" id="rua" class="form-control input-sm @error('rua') is-valid @enderror" value="{{old('rua')}}" required autocomplete="rua" autofocus placeholder="Rua D.Afonso Henriques">
                                     @error("rua")
 
                                     <span class="text-danger" role="alert">
@@ -87,7 +86,7 @@
 								<div class="col-xs-12 col-sm-12 col-md-12">
                                         <div class="form-group">
 										<label for="nif" class="">Nif:</label><br>
-			    						<input type="text" name="nif" id="nif" class="form-control input-sm @error('nif') is-valid @enderror" value="{{old('nif')}}" required autocomplete="nif" autofocus placeholder="Insira o nif">
+			    						<input type="text" name="nif" id="nif" class="form-control input-sm @error('nif') is-valid @enderror" value="{{old('nif')}}" required autocomplete="nif" autofocus placeholder="202303101">
                                         @error("nif")
 
                                         <span class="text-danger" role="alert">
@@ -104,7 +103,7 @@
                                     <div class="col-xs-6 col-sm-6 col-md-6">
 										 <div class="form-group">
 										 <label for="telefone" class="">Telefone:</label><br>
-			    				<input type="number" name="telefone" id="telefone" class="form-control input-sm @error('telefone') is-valid @enderror" value="{{old('telefone')}}" required autocomplete="telefone" autofocus placeholder="Ex: Pedro">
+			    				<input type="number" name="telefone" id="telefone" class="form-control input-sm @error('telefone') is-valid @enderror" value="{{old('telefone')}}" required autocomplete="telefone" autofocus placeholder="Ex: 919191919">
                                 @error("telefone")
 
                                 <span class="text-danger" role="alert">
@@ -118,7 +117,7 @@
                                 <div class="col-xs-6 col-sm-6 col-md-6">
                                     <div class="form-group">
 										<label for="cod_postl" class="">Codigo Postal:</label><br>
-			    				        <input type="text" name="cod_postal" id="cod_postal" class="form-control input-sm @error('cod_postal') is-valid @enderror" value="{{old('cod_postal')}}" required autocomplete="cod_postal" autofocus placeholder="Ex: Pedro">
+			    				        <input type="text" name="cod_postal" id="cod_postal" class="form-control input-sm @error('cod_postal') is-valid @enderror" value="{{old('cod_postal')}}" required autocomplete="cod_postal" autofocus placeholder="Ex: 2480-000">
                                         @error("cod_postal")
 
                                         <span class="text-danger" role="alert">
@@ -135,7 +134,7 @@
 			    				<div class="col-xs-12 col-sm-12 col-md-12">
 			    					<div class="form-group">
 										<label for="email" class="">Email:</label><br>
-			    						<input type="text" name="email" id="login" class="form-control input-sm @error('email') is-valid @enderror" value="{{old('email')}}" required autocomplete="email" autofocus placeholder="Ex: Pedro">
+			    						<input type="text" name="email" id="login" class="form-control input-sm @error('email') is-valid @enderror" value="{{old('email')}}" required autocomplete="email" autofocus placeholder="Ex: email@petstars.pt">
                                         @error("email")
 
                                         <span class="text-danger" role="alert">
@@ -147,10 +146,9 @@
 			    				<div class="col-xs-12 col-sm-12 col-md-12">
 			    					<div class="form-group">
 
-									<label for="password" class="text-info">Password:</label><br>
-			    						<input type="password" name="password" id="password" class="form-control input-sm @error('password') is-valid @enderror">
+									
 									<label for="password" class="">Password:</label><br>
-			    						<input type="password" name="password" id="password" class="form-control input-sm @error('password') is-valid @enderror" value="{{old('password')}}" required autocomplete="password" autofocus placeholder="Ex: Pedro">
+			    						<input type="password" name="password" id="password" class="form-control input-sm @error('password') is-valid @enderror" value="{{old('password')}}" required >
 
                                         @error("password")
 
@@ -183,10 +181,10 @@
                                 <input type="submit" name="register" class="btn btn-light btn-md" value="Registar">
                             </div>
 
-							<div class="form-group" id="apagar">
+							<!--<div class="form-group" id="apagar">
                                <br>
                                 <input type="reset" name="reset" class="btn btn-light btn-md" value="Apagar">
-							</div>
+							</div>-->
 							<div class="Voltar" style="margin-left:46%;">
                                <br>
 							   <button onclick="window.location.href={{route('login')}}" class="btn btn-light">Voltar</button>
