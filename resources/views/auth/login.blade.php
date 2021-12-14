382aab9f65c3fe570ab90bf800492f3804618db6
Mário <2201258@my.ipleiria.pt>
2021-12-14 09:31:15 +0000

    atualizacao navbar e login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,8 +15,8 @@
 </a>
 <div class="container">
     <div id="login-row" class="row justify-content-center">
-        <div id="login-column" class="col">
-            <div id="login-box" class="col">
+        <div id="login-column" class="col-md-6">
+            <div id="login-box" class="colmd-12">
                 <form id="login-form" class="form" action="{{ route('login') }}" method="post">
                 @csrf
                     <h3 class="text-center texto">Login</h3>
