5070d432d9a1a71f792fd16faebdf6c8ad7cb61d
João Domingos <2201266@my.ipleiria.pt>
2021-12-14 10:16:55 +0000

    Apoios BackOffice2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,13 +15,8 @@
 </a>
 <div class="container">
     <div id="login-row" class="row justify-content-center">
-<<<<<<< HEAD
                 <div id="login-column" class="col-md-6">
                     <div id="login-box" class="col-md-12">
-=======
-        <div id="login-column" class="col-md-6">
-            <div id="login-box" class="colmd-12">
->>>>>>> 68d8b6410bd1192e8a2b735808e8307fd5ab1628
                 <form id="login-form" class="form" action="{{ route('login') }}" method="post">
                 @csrf
                     <h3 class="text-center texto">Login</h3>
